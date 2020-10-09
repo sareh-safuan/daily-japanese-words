c96204645b0c08c9be3fd4de58656a0fc28787f9
Sareh Safuan <sarehsafuan@gmail.com>
2020-10-09 22:28:37 +0800

    Updates

---
+++ precache-manifest.227b04f2093dce643ec29e606bde941b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9bd6f54ed0d310e02f7bb50fcd64d7",
+    "url": "/daily-japanese-words/index.html"
+  },
+  {
+    "revision": "08b2386b4ba2687bbe7c",
+    "url": "/daily-japanese-words/static/css/main.d37f3d3a.chunk.css"
+  },
+  {
+    "revision": "075a3ebfe597cf20acb1",
+    "url": "/daily-japanese-words/static/js/2.0428e93b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/daily-japanese-words/static/js/2.0428e93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b2386b4ba2687bbe7c",
+    "url": "/daily-japanese-words/static/js/main.ddb1964f.chunk.js"
+  },
+  {
+    "revision": "821b4b20bc3c93096b0a",
+    "url": "/daily-japanese-words/static/js/runtime-main.8cf6c820.js"
+  }
+]);
